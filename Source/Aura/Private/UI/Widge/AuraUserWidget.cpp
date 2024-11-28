8b14390d322cb34750e6e90397dd74f69ade5fca
huner77 <406284935@qq.com>
2024-11-28 11:50:59 +0800

    2. Aura User Widget and Widget (28/200)

---
+++ Source/Aura/Private/UI/Widge/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright Druid Mechanics
+
+
+#include "UI/Widge/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
---
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -0,0 +1,5 @@
+// Copyright huner
+
+
+#include "UI/WidgetController/AuraWidgetController.h"
+
